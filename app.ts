90bce78a7646226040c10a5bcaffb7129e54851a
Žiga Hajšen <ziga.hajsen@gmail.com>
2020-10-18 09:58:29 +0200

    Working with Enums

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-const person: {
+/* const person: {
   name: string;
   age: number;
   hobbies: string[];
@@ -8,6 +8,23 @@ const person: {
   age: 29,
   hobbies: ['Sports', 'Cooking'],
   role: [2, 'author'],
+}; */
+
+/* const ADMIN = 0;
+const READ_ONLY = 1;
+const AUTHOR = 2; */
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
+  name: 'Žiga',
+  age: 29,
+  hobbies: ['Sports', 'Cooking'],
+  role: Role.ADMIN,
 };
 
 // person.role.push('admin'); // not caught by ts
@@ -21,4 +38,8 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
   // console.log(hobby.map());
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log('is author');
 }
