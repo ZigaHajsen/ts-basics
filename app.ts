7d5a2d0768c8714bf4c3b577c7d290c7c1c961ac
Žiga Hajšen <ziga.hajsen@gmail.com>
2020-10-18 09:35:04 +0200

    Array Types

--- app.ts
+++ app.ts
@@ -1,6 +1,14 @@
 const person = {
   name: 'Žiga',
-  age: 29
+  age: 29,
+  hobbies: ['Sports', 'Cooking'],
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
